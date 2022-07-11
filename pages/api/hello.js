cb5dea5bb83ff09076aeda0eaa1d7f4073093ef4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-07-11 15:50:19 -0400

    Install supabase

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { supabase } from "../../utils/supabaseClient";
+
+export default async function handler(req, res) {
+
+  let { data, error, status } = await supabase
+    .from('projects')
+    .select(`name`);
+
+  res.status(200).json(data);
 }
