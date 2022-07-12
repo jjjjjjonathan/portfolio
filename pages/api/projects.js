5189a986c91df942ae5698a91d24d826ee02084f
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-07-12 00:07:25 -0400

    Write SQL query to get all projects and associated skills

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -5,11 +5,17 @@ import db from "../../utils/db";
 
 export default async function handler(req, res) {
 
-  const { rows } = await db.query(`SELECT projects.*, STRING_AGG(skills.name, ',') AS skills
+  const { rows } = await db.query(`SELECT projects.*, STRING_AGG(skills.name, ',') AS skill_names, STRING_AGG(skills.id::text, ',') AS skill_ids
   FROM projects
   INNER JOIN project_skills ON projects.id = project_skills.project_id
   INNER JOIN skills ON skills.id = project_skills.skill_id
   GROUP BY projects.id;`);
 
-  res.status(200).json(rows);
+  const projects = rows.map((row) => ({
+    ...row,
+    skill_names: row.skill_names.split(','),
+    skill_ids: row.skill_ids.split(',').map((id) => parseInt(id, 10))
+  }));
+
+  res.status(200).json(projects);
 }
