ef5f89c05437443b90d3ee447956f4fc70c1499a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-07-12 00:08:27 -0400

    Delete supabase client instances

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// import { supabase } from "../../utils/supabaseClient";
 import db from "../../utils/db";
 
 export default async function handler(req, res) {
